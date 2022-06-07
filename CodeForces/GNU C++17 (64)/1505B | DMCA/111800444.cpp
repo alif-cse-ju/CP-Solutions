49527b4a0c4996aeef007ca40270c8e456c49fc6
Alif Al Hasan <48673852+alif-cse-ju@users.noreply.github.com>
2022-06-07 11:24:18 +0600

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1505B | DMCA/111800444.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int x;
+    cin>>x;
+    cout<<(x-1)%9+1<<'\n';
+    return 0;
+}
